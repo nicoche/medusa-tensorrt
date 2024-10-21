915cf55b4d57d35c28ae649972056a889800b737
Leo <106162321+aayushmittal2@users.noreply.github.com>
2024-10-21 14:36:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 python3 /app/TensorRT-LLM/examples/run.py \
     --engine_dir /app/tmp/medusa/7B/trt_engines/fp16/1-gpu/ \
     --tokenizer_dir /app/vicuna-7b-v1.3/ \
     --max_output_len=100 \
     --medusa_choices="[[0], [1], [2], [3], [4]]" \
     --temperature 1.0 \
-    --input_text "In a small village, there lived a"
+    --input_text "In a small village, there lived a curious child who wanted to explore the world."
